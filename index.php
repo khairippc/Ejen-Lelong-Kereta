120626d9b5fcae0dce92a9e2ceda2e16a7e86172
Khairi <khairi@likemethod.com>
2026-04-23 11:01:49 +0800

    letak tanda seru

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejen Lelong Pro - Buat RM1,300 Sebulan Jadi Ejen Lelong Kereta</title>
+    <title>Ejen Lelong Pro - Buat RM1,300 Sebulan Jadi Ejen Lelong Kereta!</title>
     <meta name="description" content="Kursus pertama di Malaysia tentang cara jadi ejen lelong kereta dan buat RM1,300 sebulan tanpa modal besar.">
     
     <!-- Google Fonts -->
